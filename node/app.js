c6cec690000bdf380e9208bbd0ed484afcd69911
Graeme Neath <graeme@gallowgate.org>
2020-06-16 16:48:07 +0100

    return nolimits.json data

--- node/app.js
+++ node/app.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+        fs = require('fs')
+        fs.readFile('/pvc/dashboard/nolimits.json', 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        res.send(data);
+    });
+})
 
 app.listen(port, () => console.log(`Node express service listening`))
