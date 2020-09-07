a5761377c6c28dbf9a22a52b81fa0b20422ec6af
Graeme Neath <graeme@gallowgate.org>
2020-09-07 18:21:12 +0100

    implement get and post for store endpoint

--- node/app.js
+++ node/app.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000
 
 app.get('/track', function (req, res) {
@@ -22,6 +25,33 @@ app.get('/finance', function (req, res) {
       }
       res.send(data);
   });
+})
+
+// -d 'name=<name>&info=<info> /info/store
+app.post('/store', function (req, res) {
+  name=req.body.name;
+  info=req.body.info;
+  fs = require('fs');
+  fs.writeFile('/pvc/store/'+name, info, function (err) {
+    if (err) {
+      console.log(err);
+      res.send('error');
+    }
+    res.send('ok');
+  })
+})
+
+// /info/store?name=<name>
+app.get('/store', function (req, res) {
+  name=req.query.name
+  fs = require('fs')
+  fs.readFile('/pvc/store/'+name, 'utf8', function (err,data) {
+      if (err) {
+          console.log(err);
+          res.send('');
+      }
+      res.send(data);
+  });  
 })
 
 app.listen(port, () => console.log(`Node express service listening`))
